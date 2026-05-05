3145e820845cec14f62799f893f192f7f7ab5316
Berigoo <muhamadrizqifajri2@gmail.com>
2026-05-06 02:00:29 +0700

    change addr

--- devices/modbus_manager.py
+++ devices/modbus_manager.py
@@ -11,7 +11,7 @@ class ModbusManager:
         self.slave_id_input = 1
         self.slave_id_output = 1
         self.coil_address_input = 0x0081
-        self.coil_address_output = 0x0081
+        self.coil_address_output = 1
         self.input_count = 8
         self.coil_barrier_gate = 0
         
